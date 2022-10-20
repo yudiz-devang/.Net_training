fc41b25251ba1af57e0fa6845268cb17e3101b86
yudiz-devang <113346334+yudiz-devang@users.noreply.github.com>
2022-10-20 18:13:20 +0530

    Add files via upload

---
+++ Inheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Human
+    {
+        public void walk()
+        {
+            Console.WriteLine("walking..");
+        }
+    }
+    class Men : Human
+    {
+        public void eating()
+        {
+            Console.WriteLine("Men eating food");
+        }
+    }
+    class Inheritance
+    {
+
+        static void Main(string[] args)
+        {
+            Men m1 = new Men();
+            m1.eating();
+            m1.walk();
+        }
+    }
+}
---
+++ Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Palindrome
+    {
+        public static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp;
+            Console.Write("Enter the Number: "); //121
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+           while(n>0) 
+            {
+                r = n % 10; //1,2
+                sum = (sum * 10) + r; //1,12
+                n = n / 10;//12,
+            }
+
+            if (sum == temp)
+            {
+                Console.WriteLine(temp+" Number is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(temp + " Number is not  Palindrome");
+            }
+        }
+    }
+}
