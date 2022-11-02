e2faa6c38129c36078cb954ff3e8e1261bf84d57
yudiz-devang <113346334+yudiz-devang@users.noreply.github.com>
2022-11-02 17:10:57 +0530

    Add files via upload

---
+++ Stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+namespace StackCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                //Stack
+                 Stack stack = new Stack();       
+                if(stack.isempty()){
+                    stack.Push(10);
+                    stack.Push(20);
+                    stack.Push(30);
+                    stack.Push(40);
+                    stack.Pop();
+                    Console.WriteLine($"Top Element is {stack.Peek()}");
+                    foreach(object item in stack){
+                        Console.WriteLine(item);
+                    }
+                Console.ReadKey();
+                }
+                else{
+                    Console.WriteLine("Data exists");
+                }
+                
+
+                //Queue
+
+                
+        }
+    }
+}
