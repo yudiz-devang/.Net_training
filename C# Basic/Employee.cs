e421c8aea95a2085a06550a7a92c94d1dc5779b5
yudiz - devang <devang.amrutiya@yudiz.com>
2022-12-14 13:51:51 +0530

    C# Basic

---
+++ C# Basic/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Data
+    {
+        private string name;
+        private int id;
+        private string dept;
+
+        public Data(string name,int id,string dept)
+        {
+            this.name = name;
+            this.id = id;
+            this.dept = dept;
+        }
+
+        public void printData()
+        {
+            Console.WriteLine("Name is:" + name);
+            Console.WriteLine("Id is:" + id);
+            Console.WriteLine("Department is:" + dept);
+        }
+    }
+    class Employee
+    {
+        public static void Main(string[] args)
+        {
+            Data d1 = new Data("Devang",1943,".Net");
+            d1.printData();
+        }
+    }
+}
---
+++ C# Basic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice
+{
+
+    class demo
+    {
+        public void employee(string name, int id)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo d1 = new demo();//create a instantiated
+            d1.employee("Devang", 1943); // pass name and id
+        }
+    }
+}
